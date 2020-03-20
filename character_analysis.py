78bea9bec78c9322090006470ef64b536d16ee62
Taylor <bluevalkyrie37@gmail.com>
2020-03-19 20:25:35 -0400

    set up plan for execution

---
+++ character_analysis.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+# sklearn libraries
+
+# pull in the data
+
+# clean and prepare the data
+
+# compare features and determine relationships
+
+# create the clustering model
