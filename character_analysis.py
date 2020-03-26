979acb5fa348d55a11b9e4cc17e17fa0d62d58b4
Taylor <bluevalkyrie37@gmail.com>
2020-03-25 21:35:30 -0400

    adding function to compete power

--- character_analysis.py
+++ character_analysis.py
@@ -24,22 +24,38 @@ def prepare_data(comic_df):
     property_encoding = {'DC': 0, 'Marvel': 1}
     gender_encoding = {'Male': 0, 'Female': 1, 'Neither': 2}
     side_encoding = {'Villain': 0, 'Hero': 1, 'Neither': 2, 'Both': 3, 'Antihero': 4}
-    status_encoding = {'Alien': 0, 'Android': 1, 'Demon': 2, 'Enhanced Human': 3, 'Entity': 4,
-                       'God': 5, 'Human': 6, 'Mutant/Metahuman': 7}
-    level_encoding = {'Normal': 0, 'nonspecified': 1, 'Human': 2, 'Athlete': 3, 'Superhuman': 4,
-                      'Above Superhuman': 5,'God': 6, 'Omega': 7, 'Beyond Omega': 8}
+    status_encoding = {'Human': 0, 'Enhanced Human': 1, 'Android': 2, 'Alien': 3, 'Demon': 4,
+                       'God': 5, 'Mutant/Metahuman': 6, 'Entity': 7}
+    immortal_encoding = {'No': 0, 'Semi': 1, 'Yes': 2}
+    level_encoding = {'Delta': 0, 'Epsilon': 1, 'Gamma': 2, 'Beta': 3, 'Alpha': 4,
+                      'Omega': 5, 'Sigma': 6}
 
     comic_df.Property = comic_df.Property.map(property_encoding)
     comic_df.Gender = comic_df.Gender.map(gender_encoding)
     comic_df.Side = comic_df.Side.map(side_encoding)
     comic_df.Status = comic_df.Status.map(status_encoding)
+    comic_df.Immortality = comic_df.Immortality.map(immortal_encoding)
     comic_df.Level = comic_df.Level.map(level_encoding)
 
     data_encoder = preprocessing.LabelEncoder()
-    for column in comic_df.iloc[:, 4:17]:
+    for column in comic_df.iloc[:, 5:17]:
         comic_df[column] = data_encoder.fit_transform(comic_df[column])
 
     return comic_df
+
+
+# ***add in power scores***
+def power_scores(comic_df):
+    for i, j in comic_df.iterrows():
+        base_score = ((j.Immortality * 2) + j.Innate_Powers + j.Magic_Or_Mystic_Powers) * (j.Status + 1)
+        combat_score = (j.Healing_Factor * 3) + (j.Combat_Skills + j.Use_of_Combative_Weapons) + \
+                       (j.Super_Strength + j.Super_Speed)**2
+        item_score = (j.Use_of_Enhanced_Tech * 2) + (j.Magical_or_Powerful_Weapons_Items * 3)
+        advanced_score = j.Telekinesis_Telepathy + j.Ability_to_Alter_Matter_or_Energy**2 + \
+                         j.Ability_to_Alter_Space_Time_Reality**3
+        j.Power_Score = (j.Level + 1) * (base_score + combat_score + item_score + advanced_score)
+        j.Power_Score = j.Power_Score
+    print(comic_df)
 
 
 # ***compare features and determine relationships***
@@ -106,6 +122,7 @@ def make_clusters(comic_df):
 
 
 cleaned_comic_df = prepare_data(comic_dataframe)
+power_scores(cleaned_comic_df)
 # make_plots(cleaned_comic_df)
 make_clusters(cleaned_comic_df)
 print('Process finished.')
